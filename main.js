bf928c48146ea77cc62f57b6bab198f6d64343f5
DeadPackets <deadpackets@DeadPackets-Mothership.local>
2017-12-30 13:35:32 +0400

    IM DYING PLEASE HELP ME

--- main.js
+++ main.js
@@ -2,16 +2,72 @@
 const config = require('./config.json');
 
 //HTTP Server
-const express = require('express')
+const express = require('express');
 const app = express();
+
+//HTTP Logger
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+//POST request parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}))
+
+//DB
+const initDB = require('./lib/initDB')
+const db = initDB()
+
+//Auth
+const passport = require('passport');
+const expressSession = require('express-session');
+const cookieParser = require('cookie-parser')
+const SequelizeStore = require('connect-session-sequelize')(expressSession.Store);
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+app.use(cookieParser())
+app.use(expressSession({
+  secret: config.webserver.cookieSecret,
+  httpOnly: true,
+  store: new SequelizeStore({db: db.sequelize}),
+  proxy: true,
+  resave: false,
+  saveUninitialized: true,
+  name: 'session'
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Functions
 const sendEmail = require('./lib/sendEmail');
+
+//Middleware
+const flash = require('connect-flash');
+app.use(flash());
 
 //Some optimizations
 app.disable('x-powered-by')
 app.disable('etag')
 
+//Auth HTTP
+app.get('/oauth', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}))
+
+app.get('/oauth/callback', passport.authenticate('google', {
+  successRedirect: '/app',
+  failureRedirect: '/'
+}))
+
+app.get('/logout', function(req, res) {
+  req.logout()
+  res.redirect('/')
+})
+
+//HTTP
+app.get('/', function(req, res) {
+  res.send('Who cares?')
+})
 
 //HTTP Server init
 app.listen(config.webserver.HTTP_PORT)
--- test.js
+++ test.js
@@ -1,13 +1,47 @@
 var Horseman = require('node-horseman');
-var horseman = new Horseman({
-  cookiesFile: './cookies.txt',
-  diskCache: true,
-  diskCachePath: './browsercache'
+const horseman = new Horseman({
+    cookiesFile: './cookies.txt',
+    diskCache: true,
+    diskCachePath: './browsercache'
 });
 
-horseman
-  .open('https://banner.aus.edu/axp3b21h/owa/bwckschd.p_disp_dyn_sched')
-  .cookies()
-  .log()
-  .click('#term_input_id')
-  .close();
+//const dict = require('./crndict.json')
+
+
+
+
+function parseBody(body, subject) {
+    console.log(subject)
+}
+
+
+function fetchStatus(termID, subject) {
+
+
+  horseman
+      .open('https://banner.aus.edu/axp3b21h/owa/bwckschd.p_disp_dyn_sched')
+      .waitForSelector('#term_input_id')
+      .cookies()
+      .evaluate( function(termID){
+        $('#term_input_id').val(termID)
+      }, termID)
+      .click('input[value="Submit"]')
+      .waitForNextPage()
+      .cookies()
+      .evaluate( function(subject){
+        $('#subj_id').val(subject)
+      }, subject)
+      .click('input[value="Class Search"]')
+      .waitForNextPage()
+      .html()
+      .then(function(body){
+        parseBody(body, subject)
+      })
+      .close()
+}
+
+var subjects = ['BIO', "ART", 'WRI']
+
+subjects.forEach(function(item, i){
+  fetchStatus('201820', item)
+})
