2d7cff45ca2b1535455bdf4001b3afbdf4fbdf83
DeadPackets <deadpackets@DeadPackets-Mothership.local>
2018-01-06 00:55:37 +0400

    please.....

--- main.js
+++ main.js
@@ -358,14 +358,14 @@ if (config.misc.enabled) {
     initCrawl()
   }, 10000)
 
-  // //Change Tor every 2 minutes
-  // setInterval(function() {
-  //   control.signalNewnym(function(err, status) {
-  //     if (err) {
-  //       console.log(chalk.red(err))
-  //     }
-  //     console.log(chalk.blue('Changed IP.'));
-  //   });
-  // }, 120000)
+  //Change Tor every 30 seconds
+  setInterval(function() {
+    control.signalNewnym(function(err, status) {
+      if (err) {
+        console.log(chalk.red(err))
+      }
+      console.log(chalk.blue('Changed IP.'));
+    });
+  }, 30000)
 
 }
