733e1fa16efdd0e1b3046b555933f91dd58f395a
DeadPackets <shadowblade7536@gmail.com>
2018-09-02 09:06:57 +0400

    Fix

--- client.js
+++ client.js
@@ -24,19 +24,10 @@ socket.on('disconnect', function () {
 	console.log(chalk.yellow('Disconnected from socket!'));
 });
 
-//Global Horseman
-const Horseman = require('node-horseman');
-const horsemanStatus = new Horseman({
-	diskCache: true,
-	diskCachePath: './browsercache',
-	timeout: 15000,
-	loadImages: false,
-	ignoreSSLErrors: true
-});
-
 //Functions
 function checkCRN(termID, crn, cb) {
 
+	const Horseman = require('node-horseman');
 	const horseman = new Horseman({
 		// cookiesFile: './cookies.txt',
 		diskCache: true,
@@ -88,11 +79,19 @@ function fetchStatus(termID, subject, crns) {
 
 	return new Promise(((resolve, reject) => {
 
-
+		const Horseman = require('node-horseman');
+		const horseman = new Horseman({
+			diskCache: true,
+			diskCachePath: './browsercache',
+			timeout: 15000,
+			loadImages: false,
+			ignoreSSLErrors: true
+		});
+    
 		const postData = `term_in=${termID}&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=${subject}&sel_crse=&sel_title=&sel_from_cred=&sel_to_cred=&sel_levl=%25&sel_instr=%25&sel_attr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a`;
 
 
-		horsemanStatus
+		horseman
 			.userAgent(random_useragent.getRandom())
 			.on('error', () => {
 				reject();
@@ -131,7 +130,7 @@ function fetchStatus(termID, subject, crns) {
 		/* eslint-disable require-jsdoc */
 		/* eslint-disable quotes */
 			.then((data) => {
-				resolve(data);
+				resolve(data, horseman);
 			});
 	}));
 }
@@ -147,7 +146,8 @@ socket.on(`crawlCRN_${config.misc.secret}`, (termID, subject, crns, cb) => {
 
 	console.log(chalk.blue(`Crawling subject ${subject}...`));
 
-	fetchStatus(termID, subject, crns).then((body) => {
+	fetchStatus(termID, subject, crns).then((body, horseman) => {
+		horseman.close();
 		cb(false, body);
 		console.log(chalk.blue(`Done crawling ${subject}.`));
 	}).catch(() => {
